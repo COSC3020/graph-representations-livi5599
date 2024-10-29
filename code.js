2fa4549f8f48d7db0e2216dc61cad229fa911d28
livi5599 <obrown1@uwyo.edu>
2024-10-29 17:30:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    let adjList = [];
+    for (row = 0; row < adjMatrix.length; row++) {
+        let neighborList = [];
+        for (column = 0; column < adjMatrix.length; column++) {
+            if (adjMatrix[row][column] == 1) {
+                neighborList.push(column);    
+            }
+        }
+        adjList.push(neighborList);
+    }
+    return adjList;
 }
