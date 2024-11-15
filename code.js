2a523c25a7d5410a22a2fdb360a32b5e4bb7698f
livi5599 <obrown1@uwyo.edu>
2024-11-14 17:17:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
 function convertToAdjMatrix(adjList) {
-    let adjMatrix = [];
-    for (node = 0; node < adjList.length; node++) {
-        adjMatrix.push(Array(adjList.length).fill(0));
+    let adjMatrix = []; //O(1)
+    for (node = 0; node < adjList.length; node++) { //runs n times
+        adjMatrix.push(Array(adjList.length).fill(0)); //O(n) -> O(n^2)
     }
-    for (vertex = 0; vertex < adjList.length; vertex++) {
-        for (neighbor = 0; neighbor < adjList[vertex].length; neighbor++) {
-            adjMatrix[vertex][(adjList[vertex][neighbor])] = 1;
+    for (vertex = 0; vertex < adjList.length; vertex++) { //runs n times
+        for (neighbor = 0; neighbor < adjList[vertex].length; neighbor++) { //runs n times
+            adjMatrix[vertex][(adjList[vertex][neighbor])] = 1; //O(n^2)
         }
     }
-    return adjMatrix;
+    return adjMatrix; //O(1)
 }
 
 function convertToAdjList(adjMatrix) {
